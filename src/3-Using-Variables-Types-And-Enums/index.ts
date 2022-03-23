06e05fe6d4caddc82add4f1961e84fca2deac3e7
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-23 09:33:27 -0500

    TS Lesson 3.4 - Creating, Using Typed Variables

--- src/3-Using-Variables-Types-And-Enums/index.ts
+++ src/3-Using-Variables-Types-And-Enums/index.ts
@@ -1,33 +1,18 @@
 // string, number, boolean, array, undefined, null, any
 
-let firstName: string | null;
-firstName = 'Dan';
+//let firstName: string;
+let firstName: string | null
+firstName = 'Dude';
 
 let age: number;
-age = 45;
-
-let hasPurchased = true;
+age = 42;
 
 let productNames: string[] = [];
-productNames.push('Basketball');
+let hasPurchased = true;
+
+productNames.push('Dudes');
 
 let petCount: number[] = [];
 petCount.push(5);
 
 console.log(firstName, age, hasPurchased, productNames, petCount);
-
-let productType = 'sports'; // homeGoods, grocery ("magic string")
-if (productType === 'sports') {
-  console.log('Found sports product type.');
-}
-
-// Using Enums
-enum ProductType {
-  Sports,
-  HomeGoods,
-  Groceries,
-}
-let pt = ProductType.Sports;
-if (pt === ProductType.Sports) {
-  console.log('Found sports product type.');
-}
