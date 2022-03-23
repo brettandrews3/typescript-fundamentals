6dade22ebd575a56a2737cf19cbc2ab8af64e4e9
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-23 15:00:32 -0500

    TS Lesson 4.2 - Specifying Typed Parameters

--- src/3-Using-Variables-Types-And-Enums/index.ts
+++ src/3-Using-Variables-Types-And-Enums/index.ts
@@ -16,3 +16,19 @@ let petCount: number[] = [];
 petCount.push(5);
 
 console.log(firstName, age, hasPurchased, productNames, petCount);
+
+//Lesson 3.5 - Enums
+let productType = 'sports';
+if (productType === 'sports') {
+  console.log('Found sports product type');
+}
+
+enum ProductType {
+  Sports,
+  HomeGoods,
+  Groceries
+}
+let pt = ProductType.Sports;
+if (pt === ProductType.Sports) {
+  console.log('Found sports product type again');
+}
