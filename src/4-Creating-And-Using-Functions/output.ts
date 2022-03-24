a5a24d66751cbaa8c3bb580eb74a1276193a7d4c
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-24 15:43:26 -0500

    Lesson 4.5 - Arrow Functions A
    The video started referring to a 3-digit block of code in output.ts that isn't in my version. I had to paste it in from their repo in order to continue. Lesson is in progress.

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -77,7 +77,7 @@ function runTheLearningSamples() {
   }
 
   // example of a function expression; expressions are NOT hoisted
-  const addNumbersExpression = function(x: number, y: number) {
+  const addNumbersExpression = function(x: number, y: number): number {
     const sum: number = x + y;
     return sum;
   }
@@ -85,3 +85,149 @@ function runTheLearningSamples() {
   console.log(`${prefix} function expression`);
   console.log(addNumbersExpression(64, 5));
 }
+
+// Return Scalar (in course's Git repo)
+
+// see addNumbersDeclaration (in course's Git repo)
+
+console.log(`${prefix} return scalar value`);
+//console.log(addNumbersDeclaration(7, 11));
+
+  const sampleProducts = [
+    {
+      id: 10,
+      name: 'Pizza slice',
+      icon: 'fas fa-pizza-slice',
+    },
+    {
+      id: 20,
+      name: 'Ice cream',
+      icon: 'fas fa-ice-cream',
+    },
+    {
+      id: 30,
+      name: 'Cheese',
+      icon: 'fas fa-cheese',
+    },
+  ];
+
+  function getProductNames(): string[] {
+    return sampleProducts.map((p) => p.name);
+  }
+
+  console.log(`${prefix} return array`);
+  console.log(getProductNames());
+
+  // Return Types
+
+  // CREATE type ProductType
+
+  function getProductById(id: number): ProductType | undefined {
+    return sampleProducts.find((p) => (id = p.id));
+  }
+
+  console.log(`${prefix} return ProductType`);
+  console.table(getProductById(10));
+
+  // Return void
+
+  function displayProducts(products: ProductType[]): void {
+    const productNames = products.map((p) => {
+      const name = p.name.toLowerCase();
+      return name;
+    });
+    const msg = `Sample products include: ${productNames.join(', ')}`;
+    console.log(`${prefix} return void`);
+    console.log(msg);
+  }
+
+  displayProducts(sampleProducts);
+
+  // async/await function
+
+  // *** async function getProducts()
+
+  // Arrow functions
+
+  // see function displayProducts()
+  // and layoutProducts()
+  // and getProductById()
+
+  // Optional parameters
+
+  function createProduct(name: string, icon?: string): ProductType {
+    const id = getRandomInt(1000);
+    return {
+      id,
+      name,
+      icon,
+    };
+  }
+
+  const { floor, random } = Math;
+  const getRandomInt = (max: number = 1000) => floor(random() * max);
+
+  console.log(`${prefix} Optional parameters`);
+  let pineapple = createProduct('pineapple', 'pine-apple.jpg');
+  let mango = createProduct('mango');
+  console.log(pineapple, mango);
+
+  // Default parameters
+
+  // modify getRandomInt()
+
+  function createProductWithDefaults(
+    name: string,
+    icon: string = 'generic-fruit.jpg',
+  ): ProductType {
+    const id = getRandomInt();
+    return {
+      id,
+      name,
+      icon,
+    };
+  }
+
+  console.log(`${prefix} Default parameters`);
+  pineapple = createProductWithDefaults('pineapple', 'pine-apple.jpg');
+  mango = createProductWithDefaults('mango');
+  console.log(pineapple, mango);
+
+  // *** updateOutput()
+
+  // Rest parameters
+
+  function buildAddress(
+    street: string,
+    city: string,
+    ...restOfAddress: string[]
+  ) {
+    const address = `${street}, ${city} ${restOfAddress.join(' ')}`;
+    return address;
+  }
+
+  const someAddress = buildAddress(
+    '1 lois lane',
+    'smallville',
+    'apt 101', // rest
+    'area 51', // rest
+    'mystery country', // rest
+  );
+
+  console.log(`${prefix} Rest parameters`);
+  console.log(someAddress);
+
+  // Destructuring parameters
+
+  function displayProduct({ id, name }: ProductType): void {
+    console.log(`${prefix} Destructuring parameters`);
+    console.log(`Product id=${id} and name=${name}`);
+  }
+
+  const prod = getProductById(10);
+  if (prod) {
+    displayProduct(prod);
+  }
+
+  // ~~~ Math destructuring
+  // ~~~ layoutProducts() uses destructuring
