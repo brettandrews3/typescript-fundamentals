e2aa5b6498fea19377c6daf29ff951c4a0e911fd
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-28 15:03:10 -0500

    Lesson 5.4 - Using an Interface

--- src/lib/interfaces_tutorial.ts
+++ src/lib/interfaces_tutorial.ts
@@ -4,3 +4,13 @@ export interface Product {
     icon: string,
     description?: string
 } 
+
+/*
+// Creating a sample product from the interface above
+let product: Product = {
+    id: 12,
+    name: 'Product',
+    icon: 'some.jpg',
+    description: 'The bums lost!'
+}
+*/
