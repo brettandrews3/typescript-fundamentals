4638790224c70e2b37c4d997f0035c2baf906ba1
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-28 17:42:02 -0500

    TS Lesson 6.3 - Creating and Using a Class
    Cleared out ../lib/product.ts to build fresh with the instructor. Previous commit has a working product.ts.

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,15 +1,8 @@
-import { Product } from './interfaces';
-
-// This will act as the foundation for other Product type classes (FoodProduct, SportingProduct)
-abstract class ProductBase implements Product {
-  constructor(public id: number, public name: string, public icon: string) {}
-  validate(): boolean {
-    throw new Error('Not implemented');
-  }
+// Lesson 6.3 - starting the file from scratch
+class Product {
+  // properties/fields
+  // constructor
+  // functions
 }
 
-export class FoodProduct extends ProductBase {
-  validate(): boolean {
-    return !!this.id && !!this.name && !!this.icon;
-  }
-}
+let product = new Product();
