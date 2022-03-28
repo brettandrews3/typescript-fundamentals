93c8e5bbce7c3a5315c9866ec6c88d4d1a571fdb
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-28 17:57:06 -0500

    TS Lesson 6.4 - Adding a Constructor and Properties
    Great example of using a constructor and declaring public on properties to simplify object instances.

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,8 +1,25 @@
 // Lesson 6.3 - starting the file from scratch
-class Product {
-  // properties/fields
+class FoodProduct {
+  /*
+  // properties
+  id = 0;
+  name = '';
+  icon = '';
+  
   // constructor
+  constructor(id: number, name: string, icon: string) {
+    this.id = id;
+    this.name = name;
+    this.icon = icon;
+  }
+  */
+
+  // constructor (auto implemented properties) (simplified version of above comment block)
+  // 'public' allows TypeScript to generate these properties
+  constructor(public id: number, public name: string, public icon: string) {}
+
   // functions
 }
 
-let product = new Product();
+let fp = new FoodProduct(1, 'Pizza slice', 'icon.jpg');
+
