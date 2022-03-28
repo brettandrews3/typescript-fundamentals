9b17a0e44f839dd7f7d46f9d44527408504ff3fe
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-28 16:19:32 -0500

    Lesson 5.5 - Interface vs. Type

--- src/lib/interfaces_tutorial.ts
+++ src/lib/interfaces_tutorial.ts
@@ -14,3 +14,20 @@ let product: Product = {
     description: 'The bums lost!'
 }
 */
+
+// Lesson 5.5 - example of a type alias:
+type ProductAlias = string | {
+    id: number,
+    name: string,
+    icon: string,
+    description?: string
+}
+
+let product: ProductAlias = 'Food';
+
+enum ProductType {
+    Sporting,
+    Home
+}
+
+type ProductTypeList = 'SPORTING' | 'HOME';
