2e9736985c30d747994cb3a1ffb9ec90437fb216
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-29 15:06:33 -0500

    TS Lesson 6.6 - Extending Classes, Implementing Interfaces
    Great examples of abstract class, 'extends' for child class inheritance, and 'implements' to reinforce guidelines imported from the interface file.

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,11 +1,24 @@
-//Lesson 6.5: Adding Functions
+//Lesson 6.6: Extending Classes and Implementating Interfaces
 
-export class Product {
+import { Product } from './interfaces';
+
+// class ProductBase is our building-block class
+// abstract: this is a building block class. No direct creation of instances here
+// implements: applies consistency of interface Product{} reqs: id, name, icon, desc?, bool validate()
+abstract class ProductBase implements Product {
   // Properties
   
   // Constructor
   constructor(public id: number, public name: string, public icon: string) { }
 
+  validate(): boolean {
+    throw new Error('Not implemented');
+  }
+}
+
+// extend the base class ProductBase, which applies guardrails of interface Product{}
+export class FoodProduct extends ProductBase {
+  
   // Functions
   
   // validate() makes sure that these properties have a value
@@ -15,3 +28,13 @@ export class Product {
       return !!this.id && !!this.name && !!this.icon;
   }
 }
+
+// Just an extra example from Lesson 6.6
+// Use 'extends', a JS term, to inherit from parent classes
+// Use 'super' to allow child class to use its own constructor
+class SportingGoodsProduct extends ProductBase {
+  constructor(id: number, name: string, icon: string) {
+    super(id, name, icon);
+  }
+}
+
