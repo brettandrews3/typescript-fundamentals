060d80c2d5e4b3e20612cc668a365e44f3d70112
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-04-02 11:16:26 -0500

    REDO TS Lesson 4.4 - Return Values and Types
    It reviews how to declare a new variable type (ProductType[ ]). Lesson also expands on return values, using explicit type declarations in function parameters and defining the return type (function( ) : returnType. New return types include undefined, void, and user-defined ProductType.

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -1,5 +1,12 @@
 const prefix = '🐉 ';
-// TS Lesson 4.3 - Defining Functions (hoisting, fn expressions, fn declarations)
+// TS Lesson 4.4 - Return Values (type ProductType, undefined, void return type)
+
+// TS Lesson 4.4: we're creating new type ProductType:
+type ProductType = {
+    id: number,
+    name: string,
+    icon?: string;
+};
 
 export default async function updateOutput(id: string) {
     //Ow, my toe! I STUBBED it on this function :D
@@ -31,11 +38,71 @@ function runTheLearningSamples() {
     }
     
     // Function expressions like this one are not hoisted:
-    const addNumbersExpression = function(x: number, y: number) {
+    const addNumbersExpression = function(x: number, y: number): number {
         const sum: number = x + y;
         return sum;
     }
 
     console.log(`${prefix} Number expression: NOT hoisted`);
     console.log(addNumbersExpression(16, 17));
+
+    const sampleProducts = [
+        {
+            id: 10,
+            name: 'Pizza slice',
+            icon: 'fas fa-pizza-slice',
+          },
+          {
+            id: 20,
+            name: 'Ice cream',
+            icon: 'fas fa-ice-cream',
+          },
+          {
+            id: 30,
+            name: 'Cheese',
+            icon: 'fas fa-cheese',
+          },
+    ];
+
+    // Use map() for arrays; get every item from array by using
+    // 'p' for product, then map out the name of the product ( => p.name ):
+    function getProductNames() {
+        return sampleProducts.map((p) => p.name);
+    }
+
+    console.log(`${prefix} Return of the Array`);
+    console.log(getProductNames());
+
+    // This function takes in id that is a number and return is of ProductType | undefined.
+    // undefined is needed because find() may possibly not find that object.
+    // It returns array sampleProducts with .find(), where 'p' is each item
+    // in the array. 'p' looks for the id where it is the product id (p.id).
+    function getProductById(id: number): ProductType | undefined {
+        return sampleProducts.find(p => id == p.id);
+    }
+
+    console.log(`${prefix} returning ProductType`);
+    console.log(getProductById(10));
+
+    // void return type
+    /* displayProducts() here takes in parameter products, which are type ProductType array.
+        The return type is void. The function creates productNames as the products array mapped out.
+        Again, 'p' represents items in the array. Each item 'p' is assigned to new variable name,
+        which translates to 'product name is lower case'. Return the name to the function.
+
+        Finally, declare const msg to deliver the string: 'Sample products include (list each product name,
+        joining them together with comma and space for readability). Print the result msg to the console.
+    */
+    function displayProducts(products: ProductType[]) : void {
+        const productNames = products.map(p => {
+            const name = p.name.toLowerCase();
+            return name;
+        });
+        const msg = `Sample products include: ${productNames.join(', ')}`;
+        console.log(`${prefix} return void`);
+        console.log(msg);
+    }
+
+    // displayProducts is declared just above. sampleProducts defined at ln 49 in this commit.
+    displayProducts(sampleProducts);
 }
